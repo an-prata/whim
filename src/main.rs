f5cf8be0e0564280fd6831293e06256439601321
Evan Overman <evanrileyoverman@gmail.com>
2023-07-11 12:07:47 -0700

    init new project

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+// Copyright (c) Evan Overman 2023 (https://an-prata.it).
+// Licensed under either the Apache-2.0 or MIT License.
+// See LICENSE-APACHE or LICENSE-MIT file in repository root for full text.
+
+fn main() {}
