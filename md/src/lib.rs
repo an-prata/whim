35729c6f0023424f57a42cc3b08c9644936aba3f
Evan Overman <evanrileyoverman@gmail.com>
2023-07-11 14:16:09 -0700

    it looks really ugly

--- md/src/lib.rs
+++
@@ -1,3 +0,0 @@
-// Copyright (c) Evan Overman 2023 (https://an-prata.it).
-// Licensed under either the Apache-2.0 or MIT License.
-// See LICENSE-APACHE or LICENSE-MIT file in repository root for full text.
