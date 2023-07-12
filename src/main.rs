947d8da6e9ad2cf7d045ecef51014d8901e2028b
Evan Overman <evanrileyoverman@gmail.com>
2023-07-12 13:37:12 -0700

    do the same thing just better

--- src/main.rs
+++ src/main.rs
@@ -2,40 +2,23 @@
 // Licensed under the MIT License.
 // See LICENSE file in repository root for full text.
 
-use build_html::{self, Html, HtmlContainer, HtmlPage};
-use pulldown_cmark::{self, Parser};
+mod md_content;
+
+use build_html as html;
+use html::{Html, HtmlContainer};
 
 fn main() {
-    let md = "# Some Title\nthis is a bunch or markdown\n - one\n - two\n - three\n";
-    let parser = Parser::new(md);
-    let mut parsed = String::new();
-    let title = match first_text(parser.into_iter()) {
-        Some(s) => s,
-        None => String::from(""),
-    };
-    let parser = Parser::new(md);
-    pulldown_cmark::html::push_html(&mut parsed, parser);
+    let md = "# Some Title\nthis is a bunch of markdown\n - one\n - two\n - three\n".to_string();
+    let page_content = md_content::MdContent::new(md);
+    let page_title = page_content.title().unwrap_or_default();
 
-    let mut html_document = HtmlPage::new().with_title(title);
-    html_document.add_raw(&mut parsed);
+    let content = html::Container::new(html::ContainerType::Div)
+        .with_attributes(vec![("class", "content")])
+        .with_html(page_content);
+
+    let html_document = html::HtmlPage::new()
+        .with_title(page_title)
+        .with_container(content);
+
     println!("{}", html_document.to_html_string());
-}
-
-fn first_text(parser: Parser) -> Option<String> {
-    match parser
-        .into_iter()
-        .map(|e| match e {
-            pulldown_cmark::Event::Text(cs) => Some(cs),
-            _ => None,
-        })
-        .filter(|e| e.is_some())
-        .next()
-    {
-        Some(t) => match t.unwrap() {
-            pulldown_cmark::CowStr::Boxed(s) => Some(String::from(s)),
-            pulldown_cmark::CowStr::Borrowed(s) => Some(String::from(s)),
-            pulldown_cmark::CowStr::Inlined(s) => Some(String::from(&*s)),
-        },
-        None => None,
-    }
 }
---
+++ src/md_content.rs
@@ -0,0 +1,69 @@
+// Copyright (c) Evan Overman 2023 (https://an-prata.it).
+// Licensed under the MIT License.
+// See LICENSE file in repository root for full text.
+
+use build_html as html;
+use pulldown_cmark as md;
+
+#[derive(Debug, Clone)]
+pub struct MdContent {
+    md_string: String,
+}
+
+/// Represents a peice of markdown content.
+impl MdContent {
+    /// Creates a new [`MdContent`] given a markdown string.
+    ///
+    /// [`MdContent`]: MdContent
+    #[inline]
+    #[must_use]
+    pub fn new(md_string: String) -> Self {
+        Self { md_string }
+    }
+
+    /// Gets a title from the [`MdContent`]. This looks for the first
+    /// [`Heading`] with a level of [`H1`] and then returns the first found
+    /// [`Text`] after that [`Heading`].
+    ///
+    /// [`MdDocument`]: MdDocument
+    /// [`Heading`]: md::Tag::Heading
+    /// [`H1`]: md::HeadingLevel::H1
+    /// [`Text`]: md::Event::Text
+    #[must_use]
+    pub fn title(&self) -> Option<String> {
+        let mut parser = md::Parser::new(self.md_string.as_str());
+
+        while let Some(event) = parser.next() {
+            match event {
+                // Finds the first H1 heading in the document, if it exists.
+                md::Event::Start(md::Tag::Heading(md::HeadingLevel::H1, _, _)) => {
+                    while let Some(e) = parser.next() {
+                        match e {
+                            // Return first text found after the first found H1
+                            // heading.
+                            md::Event::Text(cs) => return Some(cs.into_string()),
+                            _ => continue,
+                        }
+                    }
+
+                    // Already looped from first H1 heading to end, no need to
+                    // continue the loop.
+                    break;
+                }
+
+                _ => continue,
+            }
+        }
+
+        None
+    }
+}
+
+impl html::Html for MdContent {
+    fn to_html_string(&self) -> String {
+        let parser = md::Parser::new_ext(self.md_string.as_str(), md::Options::all());
+        let mut html_string = String::new();
+        md::html::push_html(&mut html_string, parser);
+        html_string
+    }
+}
