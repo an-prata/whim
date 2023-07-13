a10b48f068d61a6d2456787d9359d8d037119a96
Evan Overman <evanrileyoverman@gmail.com>
2023-07-12 22:43:46 -0700

    hash rework

--- src/md_content.rs
+++ src/md_content.rs
@@ -70,13 +70,7 @@ impl html::Html for MdContent {
 }
 
 impl Hashable for MdContent {
-    fn hash(self) -> u64 {
-        self.md_string.bytes().hash()
-    }
-}
-
-impl Hashable for &MdContent {
-    fn hash(self) -> u64 {
-        self.md_string.bytes().hash()
+    fn hash(&self) -> u64 {
+        self.md_string.as_bytes().hash()
     }
 }
